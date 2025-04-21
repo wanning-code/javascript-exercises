103070e841d16d1351c80e54bc7439b831fe53bd
wanning-code <198075446+wanning-code@users.noreply.github.com>
2025-04-21 20:23:55 +0200

    solution to pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argRemove) {
+    if (Array.isArray(array)) {
+        let newArray = [];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] != argRemove) {
+                newArray.push(array[i]);
+            }
+            
+        }
+        return newArray;
+    }
+    return false;
 };
 
 // Do not edit below this line
