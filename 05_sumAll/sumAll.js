eecf110f96bba51554c824ae130ac41d68095f54
wanning-code <198075446+wanning-code@users.noreply.github.com>
2025-04-22 13:50:54 +0200

    tested solution to sum all values within the range of# two Integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let temp = 0;
+  // Changed from if statement with body to Single line
+  // You can write the if statement in one line when it dont need multi line
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  if (num1 <= 0 || num2 <= 0) return "ERROR";
 
+  // An alternative way to swap the values of min and max like above 
+  // is to use the array destructuring syntax.
+  // Here's an optional article on it: 
+  // https://www.freecodecamp.org/news/array-destructuring-in-es6-30e398f21d10/
+  // if (min > max) [min, max] = [max, min];
+  if (num1 > num2) {
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+  let result = 0;
+  for (num1; num1 <= num2; num1++) {
+    result += num1;
+  }
+  return result;
 };
 
 // Do not edit below this line
