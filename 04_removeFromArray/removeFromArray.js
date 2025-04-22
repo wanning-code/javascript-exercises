d8825e33f60762bb7ad1eb892b2b1c74975c7481
wanning-code <198075446+wanning-code@users.noreply.github.com>
2025-04-22 16:22:05 +0200

    try to add removeFromArray file

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,38 @@
-const removeFromArray = function (array, ...argRemove) {
-    const sortedArray = array.sort(function (a, b) {
-      return a - b;
-    });
-    const sortedArg = argRemove.sort(function (a, b) {
-      return a - b;
-    });
-    const newArray = [];
-  
-    if (Array.isArray(array)) {
-      for (let i = 0; i < sortedArray.length; i++) {
-        if (sortedArg.includes(sortedArray[i])) {
-          console.log("continue");
-          continue;
-        }
-        newArray.push(sortedArray[i]);
+const removeFromArray = function (array, ...args) {
+//   const sortedArray = array.sort(function (a, b) {
+//     return a - b;
+//   });
+//   const sortedArg = args.sort(function (a, b) {
+//     return a - b;
+//   });
+  const newArray = [];
+  if (Array.isArray(array)) {
+    for (let i = 0; i < array.length; i++) {
+      if (args.includes(array[i])) {
+        console.log("continue");
+        continue;
       }
-      console.log(newArray);
-      return newArray;
+      newArray.push(array[i]);
     }
-  };
+    return newArray;
+  }
+};
+
+// Task solutions
+
+// const removeFromArray = function (array, ...args) {
+//   const newArray = [];
+//   array.forEach((item) => {
+//     if (!args.includes(item)) {
+//       newArray.push(item);
+//     }
+//   });
+//   return newArray;
+// };
+
+// const removeFromArray = function(array, ...args) {
+//   return array.filter(val => !args.includes(val))
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
