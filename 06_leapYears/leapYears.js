04daa9a7dc9d1d96cf0430c0bf350c10e68fa1c8
wanning-code <198075446+wanning-code@users.noreply.github.com>
2025-04-22 14:38:08 +0200

    tested solution to find out if the given year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    if (year % 4 === 0) {
+      if (year % 100 === 0) {
+        if (year % 400 === 0) return true;
+        return false;
+      }
+      return true;
+    }
+    return false;
+  };
 
-};
+// Created after sawing the solution
+// const leapYears = function (year) {
+//   if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) return true;
+//   return false;
+// };
 
 // Do not edit below this line
 module.exports = leapYears;
