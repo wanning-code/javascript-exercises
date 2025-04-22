01b49ad629d9862bc8941839ca52fa14f9152afd
wanning-code <198075446+wanning-code@users.noreply.github.com>
2025-04-22 16:18:34 +0200

    tested solutio n to convert from celsius to fahrenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  return Number(((temperature - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  return Number(((temperature * (9 / 5)) + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
